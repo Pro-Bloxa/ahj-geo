a48c805e08e46c2a2dbe30283b9c5701c4b6bcd8
Tania <taryutova@yandex.ru>
2023-05-13 21:43:21 +0300

    added test

--- src/js/__tests__/verify-coords.test.js
+++ src/js/__tests__/verify-coords.test.js
@@ -1,8 +1,12 @@
-// import validCreditCard from "../luhn-algorithm";
+import verifyCoords from "../verify-coords";
 
-// test.each([
-//   ["true for valid num", "5590135618796600", true],
-//   ["false for invalid num", "512182fg1548444023100", false],
-// ])("it should be %s", (_, input, expected) => {
-//   expect(validCreditCard(input)).toBe(expected);
-// });
+test.each([
+  ["true", "55.7640498, 37.8387726", "55.7640498, 37.8387726"],
+  ["true", "55.7640498,37.8387726", "55.7640498, 37.8387726"],
+  ["true", "[55.7640498, 37.8387726]", "55.7640498, 37.8387726"],
+  ["false", " ", false],
+  ["null", "51.50678851,−012572", false],
+])("%s", (_, coordinates, expected) => {
+  const received = verifyCoords(coordinates);
+  expect(received).toBe(expected);
+});
